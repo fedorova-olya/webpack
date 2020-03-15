0f1028b840ba6ea97186576dddc857bf566c24da
Fedorova0410 <50097427+Fedorova0410@users.noreply.github.com>
2020-03-15 19:54:57 +0200

    webpack 15.03.2020, day 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import Post from './Post'
+import * as $ from 'jquery'
+import Post from '@models/Post'
+// import json from './assets/json.json'
+// import xml from './assets/data.xml'
+// import csv from './assets/data.csv'
+import WebpackLogo from '@/assets/webpack-logo.png'
+import './style/style.css'
 
-const post = new Post ('Webpack Post Title')
+const post = new Post ('Webpack Post Title', WebpackLogo)
 
-console.log('Post to string', post.toString());
+$('pre').html(post.toString())
+// console.log('JSON: ', json);
+// console.log('XML: ', xml);
+// console.log('CSV: ', csv);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -13,10 +15,64 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
+    resolve: {
+        extensions: ['.js', '.json', '.png'],
+        alias: {
+            '@models': path.resolve(__dirname, 'src/models'),
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        }
+    },
+    devServer: {
+        port: 0,
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: './index.html'
         }),
         new CleanWebpackPlugin(),
-    ]
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, 'src/favicon.ico'),
+                to: path.resolve(__dirname, 'dist')
+            }
+        ]),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                      loader: MiniCssExtractPlugin.loader,
+                      options: {},
+                    }, 
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.csv$/,
+                use: ['csv-loader']
+            },
+        ]
+    }
 };
